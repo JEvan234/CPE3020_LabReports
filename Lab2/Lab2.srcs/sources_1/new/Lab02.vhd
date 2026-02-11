00bb39a73601d5f3ced692cf8f0c9189ec9fbbb1
Jacob Evans <Jevan234@students.kennesaw.edu>
2026-02-11 14:43:35 -0500

    Began work on lab2

---
+++ Lab2/Lab2.srcs/sources_1/new/Lab02.vhd
@@ -0,0 +1,31 @@
+----------------------------------------------------------------------------------
+-- Company: Kennesaw State University
+-- Engineer: Jacob Evans
+-- Create Date: 02/11/2026 02:06:30 PM
+-- Design Name: Lab02
+-- Module Name: Lab02 - Lab02_ARCH
+-- Project Name: Hardware Integration Lab
+-- Target Devices: Basys3 - Artix 7
+-- Description: 
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+entity Lab02 is
+  Port (
+    leftButton: in std_logic;
+    rightButton: in std_logic;
+    bitCount: in std_logic_vector(2 downto 0);
+    leftLeds: out std_logic_vector(15 downto 8);
+    rightLeds: out std_logic_vector(7 downto 0));
+end Lab02;
+
+architecture Lab02_ARCH of Lab02 is
+
+begin
+
+
+
+end Lab02_ARCH;
