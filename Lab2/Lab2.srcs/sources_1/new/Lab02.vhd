c04099dc32c128042175fdc8d9e75bb234ab8022
Jacob Evans <Jevan234@students.kennesaw.edu>
2026-02-12 16:44:16 -0500

    Began work on the component of Lab02

--- Lab2/Lab2.srcs/sources_1/new/Lab02.vhd
+++ Lab2/Lab2.srcs/sources_1/new/Lab02.vhd
@@ -10,7 +10,6 @@
 -- 
 ----------------------------------------------------------------------------------
 
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 entity Lab02 is
@@ -19,13 +18,25 @@ entity Lab02 is
     rightButton: in std_logic;
     bitCount: in std_logic_vector(2 downto 0);
     leftLeds: out std_logic_vector(15 downto 8);
-    rightLeds: out std_logic_vector(7 downto 0));
+    rightLeds: out std_logic_vector(7 downto 0);
+    whichNum: out std_logic_vector(6 downto 0));
 end Lab02;
 
 architecture Lab02_ARCH of Lab02 is
 
 begin
+    
+    SEG7_DECODER: with bitCount select
+        whichNum <= "1000000" when "000", --1
+                    "1111001" when "001", --2
+                    "0000000" when "010", --Fill in rest
+                    "0000000" when "011",
+                    "0000000" when "100",
+                    "0000000" when "101",
+                    "0000000" when "110",
+                    "0000000" when "111",
+                    "0000000" when others;
 
-
+-- Implement Left and Right Function
 
 end Lab02_ARCH;
