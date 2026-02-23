a08d10c4733ca113c136a1d5025841feedc1612f
Jacob Evans <Jevan234@students.kennesaw.edu>
2026-02-23 17:48:03 -0500

    added initial lab 3 folder with design vhdl file.

---
+++ Lab3/Lab3.srcs/sources_1/new/Lab3.vhd
@@ -0,0 +1,40 @@
+----------------------------------------------------------------------------------
+-- Company: Kennesaw State University
+-- Engineer: Jacob Evans
+-- 
+-- Create Date: 02/23/2026 05:46:14 PM
+-- Design Name: 
+-- Module Name: Lab3 - Lab3_ARCH
+-- Project Name: 
+-- Target Devices: Artix 7 - Basys 3 FPGA Board
+-- Description: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Lab3 is
+--  Port ( );
+end Lab3;
+
+architecture Lab3_ARCH of Lab3 is
+
+begin
+
+
+end Lab3_ARCH;
