e2604856e1fc553b25ded0cd2e3f34794903c1c3
Jacob Evans <Jevan234@students.kennesaw.edu>
2026-03-16 15:13:21 -0400

    Worked on lab 3

--- Lab3/Lab3.srcs/sources_1/new/Lab3.vhd
+++ Lab3/Lab3.srcs/sources_1/new/Lab3.vhd
@@ -29,12 +29,51 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity Lab3 is
---  Port ( );
+  Port (
+    leftButton : in std_logic;
+    rightButton : in std_logic;
+    clk : in std_logic;
+    reset : in std_logic;
+    leds : out std_logic_vector(15 downto 0));
 end Lab3;
 
 architecture Lab3_ARCH of Lab3 is
+-- declare the values of the lights
+constant ZERO      : std_logic_vector(15 downto 0) := "0000000000000001";
+constant ONE       : std_logic_vector(15 downto 0) := "0000000000000010";
+constant TWO       : std_logic_vector(15 downto 0) := "0000000000000100";
+constant THREE     : std_logic_vector(15 downto 0) := "0000000000001000";
+constant FOUR      : std_logic_vector(15 downto 0) := "0000000000010000";
+constant FIVE      : std_logic_vector(15 downto 0) := "0000000000100000";
+constant SIX       : std_logic_vector(15 downto 0) := "0000000001000000";
+constant SEVEN     : std_logic_vector(15 downto 0) := "0000000010000000";
+constant EIGHT     : std_logic_vector(15 downto 0) := "0000000100000000";
+constant NINE      : std_logic_vector(15 downto 0) := "0000001000000000";
+constant TEN       : std_logic_vector(15 downto 0) := "0000010000000000";
+constant ELEVEN    : std_logic_vector(15 downto 0) := "0000100000000000";
+constant TWELVE    : std_logic_vector(15 downto 0) := "0001000000000000";
+constant THIRTEEN  : std_logic_vector(15 downto 0) := "0010000000000000";
+constant FOURTEEN  : std_logic_vector(15 downto 0) := "0100000000000000";
+constant FIFTEEN   : std_logic_vector(15 downto 0) := "1000000000000000";
 
 begin
-
-
+    process(clk, reset)
+    variable value : integer range 15 downto 0;
+    begin
+        value := 0;
+        if reset = '1' then
+            value := 0;
+       
+        elsif rising_edge(clk) then
+            if rightButton = '1' then
+                value := value + 1;
+            elsif leftButton = '1' then
+                if value = 0 then
+                    value := 0;
+                else
+                    value := value - 1;
+                end if;
+            end if;
+        end if;
+    end process;
 end Lab3_ARCH;
