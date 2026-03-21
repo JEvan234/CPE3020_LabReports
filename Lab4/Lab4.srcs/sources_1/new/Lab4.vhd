5c4e60089bdb322a13fdf9008e03a9d5b8e3540c
Jacob Evans <jevan234@students.kennesaw.edu>
2026-03-21 16:55:48 -0400

    Added Lab4 Project file

---
+++ Lab4/Lab4.srcs/sources_1/new/Lab4.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/21/2026 04:54:23 PM
+-- Design Name: 
+-- Module Name: Lab4 - Lab4_ARCH
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Lab4 is
+--  Port ( );
+end Lab4;
+
+architecture Lab4_ARCH of Lab4 is
+
+begin
+
+
+end Lab4_ARCH;
